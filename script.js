2243806279a9ce7b5f99df1ac0f547b03db6bb8f
Filippo Valentini <vfilippo977@gmail.com>
2023-12-05 14:07:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,129 @@
+const questions = [
+      {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "What does CPU stand for?",
+        correct_answer: "Central Processing Unit",
+        incorrect_answers: [
+          "Central Process Unit",
+          "Computer Personal Unit",
+          "Central Processor Unit",
+        ],
+      },
+      {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+          "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+        correct_answer: "Final",
+        incorrect_answers: ["Static", "Private", "Public"],
+      },
+      {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "The logo for Snapchat is a Bell.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+      },
+      {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question:
+          "Pointers were not used in the original C programming language; they were added later on in C++.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+      },
+      {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+          "What is the most preferred image format used for logos in the Wikimedia database?",
+        correct_answer: ".svg",
+        incorrect_answers: [".png", ".jpeg", ".gif"],
+      },
+      {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "In web design, what does CSS stand for?",
+        correct_answer: "Cascading Style Sheet",
+        incorrect_answers: [
+          "Counter Strike: Source",
+          "Corrective Style Sheet",
+          "Computer Style Sheet",
+        ],
+      },
+      {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+          "What is the code name for the mobile operating system Android 7.0?",
+        correct_answer: "Nougat",
+        incorrect_answers: [
+          "Ice Cream Sandwich",
+          "Jelly Bean",
+          "Marshmallow",
+        ],
+      },
+      {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "On Twitter, what is the character limit for a Tweet?",
+        correct_answer: "140",
+        incorrect_answers: ["120", "160", "100"],
+      },
+      {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "Linux was first created as an alternative to Windows XP.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+      },
+      {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+          "Which programming language shares its name with an island in Indonesia?",
+        correct_answer: "Java",
+        incorrect_answers: ["Python", "C", "Jakarta"],
+      },
+];
+
+
+/*
+// VARIABILI UTILI: 
+let answer = []; //l'input radio dovrebbe restituire un array di elementi; accedendo alla proprietà value capiamo quale è stato selezionato (restituendolo come stringa)
+let questionNumber = []; // restituirà un array, quando > domande da fare, dovrà mostrare il punteggio
+let score = 0; 
+
+// funzione 1 - mostrare la domanda e le risposte all'utente (ordine risposte esatte casuale)
+// funzione 2 - raccogliere input utente - conta punteggio 
+// funzione 3 - restituisce il punteggio finale e lo "stampa"
+
+function showTest () {
+
+}
+
+function testAnswer (input) {
+  // array.value per capire quale risposta è stata selezionata e con ciclo FOR:
+  // IF (giusta) --> score++
+  // ELSE (sbagliata) ---> [score === score]
+}
+
+function totalScore (input) {
+  // prende la variabile in cui è salvato il valore di testAnswer
+  // restituisce il punteggio (vediamo poi se in numero o percentuale)
+}
+
+// TIMER:
+// funzione da implementare
+*/                                                                                                                                                                                                                                                                                        
